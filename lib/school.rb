bf5e62fdbb54a1c1364f1342aaf97c508e271e74
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-26 19:07:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+
+class School
+  
+  attr_accessor :roster
+ 
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    @roster[grade] = []
+    @roster[grade] << name
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+end
