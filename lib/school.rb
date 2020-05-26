1075b63f6e42c673522c191942a81d49dc950f03
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-26 19:10:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,9 +9,9 @@ class School
     @roster = {}
   end
   
-  def add_student(name, grade)
+  def add_student(student_name, grade)
     @roster[grade] || = []
-    @roster[grade] << name
+    @roster[grade] << student_name
   end
   
   def grade(grade)
